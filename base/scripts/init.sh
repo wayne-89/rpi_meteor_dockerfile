e9da2a3963a68702249562bff0ead61f33fa03e0
八九 <pangwei.pw@alibaba-inc.com>
2017-03-30 19:18:06 +0800

    tst

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -2,5 +2,5 @@ set -e
 
 bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
-bash $METEORD_DIR/lib/install_phantomjs.sh
+#bash $METEORD_DIR/lib/install_phantomjs.sh
 bash $METEORD_DIR/lib/cleanup.sh
